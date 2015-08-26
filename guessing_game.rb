911dbbda7e2400dbd6a15a01b62a2813a8b4451b
Kurtis Clauser <Kurtis@Kurtiss-MacBook-Pro.local>
2015-08-26 15:12:08 -0400

    user prompt

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,2 +1,11 @@
 random = rand(100)
-puts random
+puts "Guess a whole number between 1-100"
+
+guess = gets.chomp.to_i
+
+# while true
+#   if random == guess
+#     puts "Congratulations! You are almost as smart as me!"
+#     break
+#
+#   elsif random > guess 
