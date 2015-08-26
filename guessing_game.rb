27325cea8a5baf12484d76a3a2a5cdac42eb7400
Kurtis Clauser <Kurtis@Kurtiss-MacBook-Pro.local>
2015-08-26 15:14:03 -0400

    correct statement added

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,9 +3,9 @@ puts "Guess a whole number between 1-100"
 
 guess = gets.chomp.to_i
 
-# while true
-#   if random == guess
-#     puts "Congratulations! You are almost as smart as me!"
-#     break
-#
-#   elsif random > guess 
+while true
+  if random == guess
+    puts "Congratulations! You are almost as smart as me!"
+    break
+
+  # elsif random > guess 
